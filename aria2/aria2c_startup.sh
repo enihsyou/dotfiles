0248cc5cfb54d8548afe0f857aed8fa221395340
九条涼果 <s1131234@gmail.com>
2018-11-10 12:36:09 +0800

    add new mount NAS disk

--- aria2/aria2c_startup.sh
+++ aria2/aria2c_startup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 sudo mount -t drvfs '\\ENIHSYOU_NAS\Storage' /mnt/m
 sudo mount -t drvfs '\\ENIHSYOU_NAS\PT' /mnt/n
+sudo mount -t drvfs '\\ENIHSYOU_NAS\ExternalH' /mnt/o
 aria2c -D
