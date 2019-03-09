700f6421393d71571df933e36a606d15f66a78ae
Ryoka Kujo <s1131234@gmail.com>
2019-03-09 15:11:55 +0800

    free up disk space before RAM disk backup

--- macOS/backupRamDisk.sh
+++ macOS/backupRamDisk.sh
@@ -2,9 +2,12 @@
 DISKNAME=${1-RAM}
 IMAGE_LOCATION=${2-"/Users/enihsyou/Library/RAM.dmg"}
 DISK_LOCATION=/Volumes/${DISKNAME}
-#INFO=`diskutil info /Volumes/${DISKNAME} | sed -n -E 's/^.*Part of Whole:.*(disk.*[0-9])/\1/p'`
 
-#hdiutil detach $INFO
+# Free up disk space before backup
+# delete all Chrome user caches
+rm -rf $DISK_LOCATION/Cache/Chrome/*
+# clear compile caches
+rm -f $DISK_LOCATION/Compile/*
 
 sudo hdiutil create -srcfolder $DISK_LOCATION $IMAGE_LOCATION -ov
 sudo chown enihsyou $IMAGE_LOCATION
