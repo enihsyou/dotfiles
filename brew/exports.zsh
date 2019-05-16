a22a999718dc3629e5813466dd3d276f20ae1976
Ryoka Kujo <s1131234@gmail.com>
2019-05-16 17:49:53 +0800

    tmux enhancement

--- brew/exports.zsh
+++ brew/exports.zsh
@@ -1,3 +1,3 @@
 # Specify your defaults in this environment variable
-export HOMEBREW_CASK_OPTS="--appdir=~/Applications --fontdir=/Library/Fonts"
+export HOMEBREW_CASK_OPTS="--fontdir=~/Library/Fonts"
 export HOMEBREW_NO_AUTO_UPDATE=1
