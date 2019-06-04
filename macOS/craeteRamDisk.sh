923eb944595660b45d2e7e4862f3fd2936180f4f
Ryoka Kujo <s1131234@gmail.com>
2019-06-05 02:14:20 +0800

    修复不创建自定义名字的错误
    在手动创建内存盘时不还原镜像
    应用Shellcheck的提示
    添加Debug flag

--- macOS/craeteRamDisk.sh
+++ macOS/craeteRamDisk.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
+
+[ -n "$DEBUG" ] && set -x
+
 SIZE=${1-2048}
 DISKNAME=${2-RAM}
 
 BASE=/Volumes/$DISKNAME
 [[ -e $BASE ]] && exit 0
 
-DEVICE_IDENTIFIER=`diskutil info $DISKNAME | sed -n -E 's/^.*Device Identifier:.*(disk.*[0-9]).*/\1/p'`
+DEVICE_IDENTIFIER=$( diskutil info "$DISKNAME" | sed -n -E 's/^.*Device Identifier:.*(disk.*[0-9]).*/\1/p' )
 if [[ -n $DEVICE_IDENTIFIER ]]; then
-    hdiutil mountvol $DEVICE_IDENTIFIER
+    hdiutil mountvol "$DEVICE_IDENTIFIER"
     exit 0
 fi
 
-DISK_ID=`hdiutil attach -nomount ram://$(( ${SIZE} * 1024 * 1024 / 512 ))` # a sector is 512 bytes
-diskutil partitionDisk $DISK_ID GPT APFS "$DISKNAME" 0
+DISK_ID=$( hdiutil attach -nomount ram://$(( SIZE * 1024 * 1024 / 512 )) | sed -e 's/[[:space:]]*$//' ) # a sector is 512 bytes
+diskutil partitionDisk "$DISK_ID" GPT APFS "$DISKNAME" 0
 
 # Oneline version
 #diskutil partitionDisk `hdiutil attach -nomount ram://8388608` GPT APFS "RAM" 0
 
 # Backup location
 IMAGE_LOCATION=${3-"/Users/enihsyou/Library/RAM.dmg"}
-if [ -e $IMAGE_LOCATION ]; then
+if [ "$DISKNAME" != "RAM" ]; then
+    exit 0
+elif [ -e "$IMAGE_LOCATION" ]; then
     # asr imagescan --source $IMAGE_LOCATION --nostream
-    asr restore --source $IMAGE_LOCATION --target $BASE --erase --noverify --noprompt
+    asr restore --source "$IMAGE_LOCATION" --target "$BASE" --erase --noverify --noprompt
 else
-    cd $BASE
+    cd "$BASE" || exit 1
     mkdir -p Cache/{Chrome,Firefox}
     mkdir -p Compile
     mkdir -p Temp/logs
