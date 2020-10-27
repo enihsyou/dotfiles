fff6556245bca7d72f3fc206454b7f81d80c16d5
Ryoka Kujo <s1131234@gmail.com>
2020-10-27 14:35:39 +0800

    一些brew环境配置，避免下载失败后从源码构建

--- brew/exports.zsh
+++ brew/exports.zsh
@@ -1,8 +1,14 @@
-# Specify your defaults in this environment variable
+# Specify brew defaults in this environment variable
+# https://docs.brew.sh/Manpage#environment
+export HOMEBREW_BAT=1
 export HOMEBREW_CASK_OPTS="--fontdir=~/Library/Fonts"
 export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_CLEANUP_MAX_AGE_DAYS=45
+export HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK=1
 #export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
-#export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+export HOMEBREW_BREW_GIT_REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+export HOMEBREW_CORE_GIT_REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
 
 # export brew linked binary
 export PATH="/usr/local/sbin:$PATH:~/.local/bin"
