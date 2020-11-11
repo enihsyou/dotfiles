9946aa75a98b17fe51d1464a79ee895c62fd9488
Ryoka Kujo <s1131234@gmail.com>
2020-11-11 15:33:26 +0800

    添加更多兼容性配置，允许在Linux上使用

--- brew/exports.zsh
+++ brew/exports.zsh
@@ -6,10 +6,11 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_CLEANUP_MAX_AGE_DAYS=45
 export HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK=1
 #export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
-export HOMEBREW_BREW_GIT_REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
-export HOMEBREW_CORE_GIT_REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+#export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+#export HOMEBREW_BREW_GIT_REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
+#export HOMEBREW_CORE_GIT_REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
 
+if [ "$(uname)" = Darwin ]; then
 # export brew linked binary
 export PATH="/usr/local/sbin:$PATH:~/.local/bin"
 
@@ -20,3 +21,4 @@ export PATH="/usr/local/opt/python@3.8/bin:$PATH"
 export NODEJS_HOME="/usr/local/opt/node"
 # export brew installed yarn home
 export YARN_HOME="/usr/local/opt/yarn"
+fi
