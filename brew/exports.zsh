177a3d56a6f8d132febb843f6d384ee8224190ef
Ryoka Kujo <s1131234@gmail.com>
2021-01-04 01:37:38 +0800

    更加环境中立的brew配置

--- brew/exports.zsh
+++ brew/exports.zsh
@@ -1,11 +1,16 @@
 # Specify brew defaults in this environment variable
 # https://docs.brew.sh/Manpage#environment
+
 export HOMEBREW_BAT=1
-export HOMEBREW_CASK_OPTS="--fontdir=~/Library/Fonts"
+export HOMEBREW_CURLRC=1
+#export HOMEBREW_VERBOSE=1
+#export HOMEBREW_VERBOSE_USING_DOTS=1
+export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_CLEANUP_MAX_AGE_DAYS=45
+export HOMEBREW_DISPLAY_INSTALL_TIMES=1
 export HOMEBREW_NO_BOTTLE_SOURCE_FALLBACK=1
-#export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
 export HOMEBREW_BREW_GIT_REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
 export HOMEBREW_CORE_GIT_REMOTE=https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
@@ -14,11 +19,14 @@ if [ "$(uname)" = Darwin ]; then
 # export brew linked binary
 export PATH="/usr/local/sbin:$PATH:~/.local/bin"
 
-# export brew installed python@3.8
-export PATH="/usr/local/opt/python@3.8/bin:$PATH"
-
 # export brew installed nodejs home
 export NODEJS_HOME="/usr/local/opt/node"
 # export brew installed yarn home
 export YARN_HOME="/usr/local/opt/yarn"
+fi
+
+# set temp folder into ram disk
+if [[ -e /Volumes/RAM ]]; then
+    mkdir -p /Volumes/RAM/Cache
+    export HOMEBREW_TEMP="/Volumes/RAM/Cache"
 fi
