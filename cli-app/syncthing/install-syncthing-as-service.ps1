4124a4cbb00d1139839fbf69d00e8205f7c73f41
九条涼果 <enihsyou@gmail.com>
2025-05-21 17:44:02 +0800

    参考 gist 实现修改

--- cli-app/syncthing/install-syncthing-as-service.ps1
+++ cli-app/syncthing/install-syncthing-as-service.ps1
@@ -1,64 +1,9 @@
 ﻿# 使用 nssm 安装 syncthing 服务
 # https://docs.syncthing.net/users/autostart.html#autostart-windows-taskschd
 
-# Stop execution when something goes wrong
-$ErrorActionPreference = "Stop"
-
 # 环境变量在 sudo 时也要保证注入
 Import-Module -Name "$env:DOTFILES\shell\pwsh\Modules\NssmHelper.psm1" -Force -DisableNameChecking
 
-Require-Sudo $($MyInvocation.MyCommand.Path)
-
-# 检查必要的命令和文件
-$requiredCommands = @('syncthing', 'nssm')
-
-# 检查命令
-foreach ($cmd in $requiredCommands) {
-    Assert-CommandExists $cmd
-}
-
-# 获取命令路径
-$syncthingPath = Get-CommandPath 'syncthing'
-$nssmPath = Get-CommandPath 'nssm'
-
-# 设置服务名称
-$serviceName = 'syncthing'
-
-# 检查服务是否存在
-Remove-ExistingService $serviceName
-
-# 安装服务
-try {
-    # 使用 NSSM 安装服务
-    & $nssmPath install $serviceName $syncthingPath "--no-restart" "--no-browser"
-
-    # 设置服务属性
-    # https://nssm.cc/commands
-    $nssmSettings = @{
-        DisplayName     = "Syncthing Service [managed by nssm]"
-        Description     = "Syncthing is a continuous file synchronization program."
-        Start           = "SERVICE_DELAYED_AUTO_START"
-        DependOnService = "Tcpip"
-        AppDirectory    = $PSScriptRoot
-        AppNoConsole    = "1"
-    }
-
-    foreach ($setting in $nssmSettings.GetEnumerator()) {
-        & $nssmPath set $serviceName $setting.Key $setting.Value
-    }
-
-    # 根据文档为处理重启更新行为需要额外设置的属性
-    # https://docs.syncthing.net/users/autostart.html#autostart-windows-taskschd
-    & $nssmPath set $serviceName AppExit Default Exit
-    & $nssmPath set $serviceName AppExit 0 Exit
-    & $nssmPath set $serviceName AppExit 3 Restart
-    & $nssmPath set $serviceName AppExit 4 Restart
-
-    # 启动服务
-    Start-Service -Name $serviceName
-
-    Write-Host "$($serviceName) 服务已成功安装并启动！" -ForegroundColor Green
-} catch {
-    Write-Error "安装或配置服务时出错：$_"
-    exit 1
-}
+# 权限自提升
+$SudoScript = Join-Path $PSScriptRoot 'install-syncthing-as-service.sudo.ps1'
+Require-Sudo $SudoScript
---
+++ cli-app/syncthing/install-syncthing-as-service.sudo.ps1
@@ -0,0 +1,92 @@
+﻿<#
+.SYNOPSIS
+    Installs Syncthing as a Windows service
+.DESCRIPTION
+    Uses NSSM (Non-Sucking Service Manager) to set up Syncthing as a Windows
+    service. Requires both Syncthing and NSSM to already be installed. nssm.exe
+    must be available via the PATH environment variable.
+.PARAMETER ServiceName
+    Defaults to "Syncthing"
+.NOTES
+    Reference: https://gist.github.com/pcrockett-pathway/76f5cbdd578c17ef2a12a804959f2060
+#>
+
+#Requires -RunAsAdministrator
+[CmdletBinding()]
+param(
+    [Parameter()]
+    [string]$ServiceName = "Syncthing"
+)
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version 3.0
+
+if (!(Get-Command "nssm" -ErrorAction SilentlyContinue)) {
+    throw "nssm.exe not found. Make sure it is included in the PATH environment variable."
+}
+
+# get full path to syncthing.exe
+$syncthingExe = Get-Command syncthing.exe -ErrorAction SilentlyContinue
+if (!$syncthingExe) {
+    throw "syncthing.exe not found. Make sure it is included in the PATH environment variable."
+}
+$syncthingExe = $syncthingExe.Source
+
+if (Get-Service -Name $ServiceName -ErrorAction SilentlyContinue) {
+    Write-Host "Removing existing service $ServiceName ..." -ForegroundColor Yellow
+    Stop-Service -Name $ServiceName -Force -ErrorAction SilentlyContinue
+    Remove-Service -Name $ServiceName -Force -ErrorAction SilentlyContinue
+    $waitingSecond = 0
+    while (Get-Service -Name $ServiceName -ErrorAction SilentlyContinue) {
+        Write-Host "." -NoNewline
+        Start-Sleep -Seconds 1
+        $waitingSecond++
+        if ($waitingSecond -ge 10) {
+            throw "Removing service $ServiceName timed out. Please check if the service is still in use, for example, if there are no windows still browsing it."
+        }
+    }
+}
+
+$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$serviceCreds = Get-Credential -UserName $currentUser -Message "The Syncthing service should run under the current user to reduce security risks."
+
+# most settings can be obtained by: nssm dump <serviceName>
+$nssmSettings = @{
+    AppParameters   = "--no-restart --no-browser --no-console"
+    DisplayName     = "Syncthing Service [managed by nssm]"
+    Description     = "Syncthing is a continuous file synchronization program."
+    Start           = "SERVICE_DELAYED_AUTO_START"
+    DependOnService = "Tcpip"
+    AppNoConsole    = "1"
+    AppExit         = @{
+        # https://docs.syncthing.net/users/autostart.html#autostart-windows-taskschd
+        Default = "Exit"
+        1       = "Restart"
+        2       = "Restart"
+        3       = "Restart"
+        4       = "Restart"
+    }
+    ObjectName      = @{
+        $serviceCreds.UserName = $serviceCreds.GetNetworkCredential().Password
+    }
+}
+
+function Invoke-Nssm {
+    nssm $args
+    if ($LASTEXITCODE -ne 0) {
+        throw "nssm exited with code $LASTEXITCODE"
+    }
+}
+
+Invoke-Nssm install $ServiceName $syncthingExe
+foreach ($setting in $nssmSettings.GetEnumerator()) {
+    if ($setting.Value -is [hashtable]) {
+        foreach ($subSetting in $setting.Value.GetEnumerator()) {
+            Invoke-Nssm set $ServiceName $setting.Key $subSetting.Key $subSetting.Value
+        }
+    } else {
+        Invoke-Nssm set $ServiceName $setting.Key $setting.Value
+    }
+}
+
+Start-Service -Name $ServiceName
