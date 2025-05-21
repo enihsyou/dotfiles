df9ca15bb9124a67b979eb0ac938c7e567a41038
九条涼果 <enihsyou@gmail.com>
2025-05-21 15:26:45 +0800

    Install syncthing as service

--- cli-app/syncthing/install-syncthing-as-service.ps1
+++ cli-app/syncthing/install-syncthing-as-service.ps1
@@ -1,4 +1,5 @@
-# 使用 nssm 安装 aria2 服务
+﻿# 使用 nssm 安装 syncthing 服务
+# https://docs.syncthing.net/users/autostart.html#autostart-windows-taskschd
 
 # Stop execution when something goes wrong
 $ErrorActionPreference = "Stop"
@@ -9,30 +10,19 @@ Import-Module -Name "$env:DOTFILES\shell\pwsh\Modules\NssmHelper.psm1" -Force -D
 Require-Sudo $($MyInvocation.MyCommand.Path)
 
 # 检查必要的命令和文件
-$requiredCommands = @('aria2c', 'nssm')
-$requiredFiles = @(
-    @{
-        Path = Join-Path $PSScriptRoot 'aria2.conf'
-        Name = 'aria2.conf'
-    }
-)
+$requiredCommands = @('syncthing', 'nssm')
 
 # 检查命令
 foreach ($cmd in $requiredCommands) {
     Assert-CommandExists $cmd
 }
 
-# 检查文件
-foreach ($file in $requiredFiles) {
-    Assert-FileExists $file.Path
-}
-
 # 获取命令路径
-$aria2Path = Get-CommandPath 'aria2c'
+$syncthingPath = Get-CommandPath 'syncthing'
 $nssmPath = Get-CommandPath 'nssm'
 
 # 设置服务名称
-$serviceName = 'aria2'
+$serviceName = 'syncthing'
 
 # 检查服务是否存在
 Remove-ExistingService $serviceName
@@ -40,13 +30,13 @@ Remove-ExistingService $serviceName
 # 安装服务
 try {
     # 使用 NSSM 安装服务
-    & $nssmPath install $serviceName $aria2Path "--conf-path=$($requiredFiles[0].Path)"
+    & $nssmPath install $serviceName $syncthingPath "--no-restart" "--no-browser"
 
     # 设置服务属性
     # https://nssm.cc/commands
     $nssmSettings = @{
-        DisplayName     = "Aria2 Download Service [managed by nssm]"
-        Description     = "aria2 is a lightweight multi-protocol & multi-source, cross platform download utility operated in command-line. It supports HTTP/HTTPS, FTP, SFTP, BitTorrent and Metalink."
+        DisplayName     = "Syncthing Service [managed by nssm]"
+        Description     = "Syncthing is a continuous file synchronization program."
         Start           = "SERVICE_DELAYED_AUTO_START"
         DependOnService = "Tcpip"
         AppDirectory    = $PSScriptRoot
@@ -56,6 +46,13 @@ try {
     foreach ($setting in $nssmSettings.GetEnumerator()) {
         & $nssmPath set $serviceName $setting.Key $setting.Value
     }
+
+    # 根据文档为处理重启更新行为需要额外设置的属性
+    # https://docs.syncthing.net/users/autostart.html#autostart-windows-taskschd
+    & $nssmPath set $serviceName AppExit Default Exit
+    & $nssmPath set $serviceName AppExit 0 Exit
+    & $nssmPath set $serviceName AppExit 3 Restart
+    & $nssmPath set $serviceName AppExit 4 Restart
 
     # 启动服务
     Start-Service -Name $serviceName
