f0794a300d551b44bf4aa13834312674483dc5bb
九条涼果 <enihsyou@gmail.com>
2025-09-22 18:42:16 +0800

    sudo edit in yazi

--- shell/bash/bashrc-wsl2.sh
+++ shell/bash/bashrc-wsl2.sh
@@ -130,7 +130,8 @@ function x() {
 }
 
 function ble() {
-    # install before using:
+    # enable auto completion by execute `ble` on current shell.
+    # make sure to install before using:
     # curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
     # bash ble-nightly/ble.sh --install ~/.local/share
     [[ $- == *i* ]] && source -- "$HOME/.local/share/blesh/ble.sh" --rcfile "$DOTFILES/cli-app/blesh/blerc"
