5bb88552048db31cab3ffa280e537fc0c9920ef9
九条涼果 <enihsyou@gmail.com>
2026-04-03 22:38:43 +0800

    Create Gemini.app with Pake

---
+++ gui-app/Pake/Windows.ps1
@@ -0,0 +1,5 @@
+# 使用小于 960px 的宽度，默认不显示侧边栏
+pake https://gemini.google.com/ --name Gemini --width 959 --height 1280 `
+  --enable-drag-drop --installer-language zh-CN `
+  --activation-shortcut "Super+Alt+C" `
+  --inject ./gemini.css
